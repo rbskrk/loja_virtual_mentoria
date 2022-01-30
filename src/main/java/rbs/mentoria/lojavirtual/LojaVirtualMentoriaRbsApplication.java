ddff25af3fc08f4e338fdae10e7f2f3c7c11255d
= <=>
2022-01-30 20:46:02 -0300

    commit inicial projeto mentoria Java

---
+++ src/main/java/rbs/mentoria/lojavirtual/LojaVirtualMentoriaRbsApplication.java
@@ -0,0 +1,13 @@
+package rbs.mentoria.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualMentoriaRbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualMentoriaRbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rbs/mentoria/lojavirtual/LojaVirtualMentoriaRbsApplicationTests.java
@@ -0,0 +1,13 @@
+package rbs.mentoria.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualMentoriaRbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
