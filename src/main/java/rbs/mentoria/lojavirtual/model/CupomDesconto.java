fd9d1f5a5e16da29b0bcd668c0ca674414a3068c
= <=>
2022-02-04 21:21:35 -0300

    commit classe produto

---
+++ src/main/java/rbs/mentoria/lojavirtual/model/CupomDesconto.java
@@ -0,0 +1,95 @@
+package rbs.mentoria.lojavirtual.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@SequenceGenerator(name = "seq_cupom_desconto", sequenceName = "seq_cupom_desconto", allocationSize = 1, initialValue = 1)
+@Table(name = "cupom_desconto")
+public class CupomDesconto implements Serializable  {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator ="seq_cupom_desconto")
+	private Long id;
+	
+	private String codDesconto;
+	
+	private BigDecimal valorRealDesconto;
+	
+	private BigDecimal valorPorcentagemDesconto;
+			
+	@Temporal(TemporalType.DATE)		
+	private Date dataValidadeCupom;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCodDesconto() {
+		return codDesconto;
+	}
+
+	public void setCodDesconto(String codDesconto) {
+		this.codDesconto = codDesconto;
+	}
+
+	public BigDecimal getValorRealDesconto() {
+		return valorRealDesconto;
+	}
+
+	public void setValorRealDesconto(BigDecimal valorRealDesconto) {
+		this.valorRealDesconto = valorRealDesconto;
+	}
+
+	public BigDecimal getValorPorcentagemDesconto() {
+		return valorPorcentagemDesconto;
+	}
+
+	public void setValorPorcentagemDesconto(BigDecimal valorPorcentagemDesconto) {
+		this.valorPorcentagemDesconto = valorPorcentagemDesconto;
+	}
+
+	public Date getDataValidadeCupom() {
+		return dataValidadeCupom;
+	}
+
+	public void setDataValidadeCupom(Date dataValidadeCupom) {
+		this.dataValidadeCupom = dataValidadeCupom;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CupomDesconto other = (CupomDesconto) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+
+}
---
+++ src/main/java/rbs/mentoria/lojavirtual/model/Produto.java
@@ -0,0 +1,192 @@
+package rbs.mentoria.lojavirtual.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@SequenceGenerator(name = "seq_produto", sequenceName = "seq_produto", allocationSize = 1, initialValue = 1)
+public class Produto implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_produto")
+	private Long Id;
+	
+	private String tipoUnidade;
+	
+	private String nome;
+	
+	@Column(columnDefinition = "text", length = 2000)
+	private String decricao;
+	
+	private Boolean ativo = Boolean.TRUE;
+	
+	private Double peso;
+	
+	private Double largura;
+	
+	private Double altura;
+	
+	private Double profundidade;
+	
+	private BigDecimal valorVenda = BigDecimal.ZERO;
+	
+	private Integer quantidadeEstoque= 0;
+	
+	private Integer quantAlertaEstoque= 0;;
+	
+	private String linkYouTube;
+	
+	private Boolean quantEstoque = Boolean.FALSE;
+	
+	private Integer qntDeClique = 0;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getTipoUnidade() {
+		return tipoUnidade;
+	}
+
+	public void setTipoUnidade(String tipoUnidade) {
+		this.tipoUnidade = tipoUnidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDecricao() {
+		return decricao;
+	}
+
+	public void setDecricao(String decricao) {
+		this.decricao = decricao;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	public Double getLargura() {
+		return largura;
+	}
+
+	public void setLargura(Double largura) {
+		this.largura = largura;
+	}
+
+	public Double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+
+	public Double getProfundidade() {
+		return profundidade;
+	}
+
+	public void setProfundidade(Double profundidade) {
+		this.profundidade = profundidade;
+	}
+
+	public BigDecimal getValorVenda() {
+		return valorVenda;
+	}
+
+	public void setValorVenda(BigDecimal valorVenda) {
+		this.valorVenda = valorVenda;
+	}
+
+	public Integer getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+
+	public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+
+	public Integer getQuantAlertaEstoque() {
+		return quantAlertaEstoque;
+	}
+
+	public void setQuantAlertaEstoque(Integer quantAlertaEstoque) {
+		this.quantAlertaEstoque = quantAlertaEstoque;
+	}
+
+	public String getLinkYouTube() {
+		return linkYouTube;
+	}
+
+	public void setLinkYouTube(String linkYouTube) {
+		this.linkYouTube = linkYouTube;
+	}
+
+	public Boolean getQuantEstoque() {
+		return quantEstoque;
+	}
+
+	public void setQuantEstoque(Boolean quantEstoque) {
+		this.quantEstoque = quantEstoque;
+	}
+
+	public Integer getQntDeClique() {
+		return qntDeClique;
+	}
+
+	public void setQntDeClique(Integer qntDeClique) {
+		this.qntDeClique = qntDeClique;
+	}
+	
+	public Boolean getAtivo() {
+		return ativo;
+	}
+	
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(Id, other.Id);
+	}
+	
+	
+
+}
